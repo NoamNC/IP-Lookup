50a3a980a61848b1a626c024a48264075d479814
Noam Cohen <noamnc@gmail.com>
2022-05-30 10:46:28 +0300

    exported navbar to its own component

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-btn href="https://torq.io/" target="_blank" text>
-          <v-img
-            alt="Torq logo"
-            class="shrink"
-            contain
-            min-width="100"
-            src="https://torq.io/wp-content/uploads/2021/06/torq-blck.png"
-            width="100"
-          />
-        </v-btn>
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/NoamNC"
-        target="_blank"
-        text
-        class="btn-text shrink"
-      >
-        <span class="side-text">Checkout my other projects</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+    <NavBar />
     <v-main>
       <IpLookup />
     </v-main>
@@ -35,10 +9,13 @@
 
 <script>
 import IpLookup from './components/IpLookup.vue';
+import NavBar from './components/NavBar.vue';
+
 export default {
   name: 'App',
   components: {
-    IpLookup,
+    NavBar,
+    IpLookup
   },
 };
 </script>
@@ -48,8 +25,5 @@ export default {
   text-transform: none;
 }
 
-.btn-text {
-  display: inline-block;
-  white-space: normal;
-}
+
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <div class="d-flex align-center">
+      <v-btn href="https://torq.io/" target="_blank" text>
+        <v-img
+          alt="Torq logo"
+          class="shrink"
+          contain
+          min-width="100"
+          src="https://torq.io/wp-content/uploads/2021/06/torq-blck.png"
+          width="100"
+        />
+      </v-btn>
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn href="https://github.com/NoamNC?tab=repositories" target="_blank" text class="btn-text shrink">
+      <span class="side-text">Check out my other projects</span>
+      <v-icon>mdi-open-in-new</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'TopBar',
+};
+</script>
+
+<style lang="css" scoped>
+.btn-text {
+  display: inline-block;
+  white-space: normal;
+}
+</style>
